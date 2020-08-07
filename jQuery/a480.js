1898be0cf424aeae11478627392331567e864b43
kcpedrosa <kattine.costa@gmail.com>
2020-08-07 15:01:16 -0300

    Adding jquery test files

---
+++ jQuery/a480.js
@@ -0,0 +1,22 @@
+console.log('good request')
+
+//use this fist notation, more common
+$(document).ready(function(){
+	function teste(){
+		console.log($('#teste'))
+	}
+	teste()
+
+	})
+	
+
+$(function(){
+		console.log($('#exemplo'))
+	})
+
+	
+function code(){
+		console.log($( '#napster' ))
+	}
+
+$(code)
